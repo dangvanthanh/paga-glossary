45f3f3350cd1c1366e4f519867cf7eed5fc4425c
Dang Van Thanh <dangvanthanh@dangthanh.org>
2024-02-05 20:55:28 +0700

    feat: update panda config

--- panda.config.ts
+++ panda.config.ts
@@ -2,23 +2,14 @@ import { defineConfig } from '@pandacss/dev'
 
 export default defineConfig({
 	lightningcss: true,
-	// Whether to use css reset
 	preflight: true,
-
-	// Where to look for your css declarations
 	include: [
 		'./src/**/*.{js,jsx,ts,tsx,astro}',
 		'./pages/**/*.{js,jsx,ts,tsx,astro}',
 	],
-
-	// Files to exclude
 	exclude: [],
-
-	// Useful for theme customization
 	theme: {
 		extend: {},
 	},
-
-	// The output directory for your css system
 	outdir: 'styled-system',
 })
